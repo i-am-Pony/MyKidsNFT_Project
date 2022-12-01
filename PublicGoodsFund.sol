119b02bdef351802baf6cbef69ca9dbf33402c6c
i-am-Pony <109096522+i-am-Pony@users.noreply.github.com>
2022-11-30 22:56:39 -0500

    Update PublicGoodsFund.sol

--- PublicGoodsFund.sol
+++ PublicGoodsFund.sol
@@ -16,12 +16,12 @@ function donate() public payable{
 // this withdraw function has a secure payout structure that is only for use by "OnlyOwner"
 // we added a second payout address for charity / donations to Public Goods Fund
         function withdraw() public payable onlyOwner {
-        /// This will donate 20% of all donations to Optimism. ((In theory, it is currently set to the 
+        /// This will donate 10% of all donations to Optimism. ((In theory, it is currently set to the 
         // Optimism "Contract" we will change this before we deploy on mainnet, we need to
         // obtain an address for donating to pblic goods funds from Optimism.
-        (bool hs, ) = payable(0x4200000000000000000000000000000000000042).call{value: address(this).balance * 60 / 100}("Optimism");
+        (bool hs, ) = payable(0x4200000000000000000000000000000000000042).call{value: address(this).balance * 10 / 100}("Optimism");
         require(hs);
-        /// This will send the Community Donation Wallet the remaining 80% of all donations for
+        /// This will send the Community Donation Wallet the remaining 90% of all donations for
         // distrubution to community chosen charity
         (bool os, ) = payable(0xfA133DB2F7d904408AF94129260b4a6799777D54).call{value: address(this).balance}("Charity");
         require(os);
